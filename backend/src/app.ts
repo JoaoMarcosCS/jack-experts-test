281000c9edacfe2c6c12b8b25a84390f988373ab
JoaoMarcosCS <jmcsjoaomarcos@gmail.com>
2024-08-26 17:43:46 -0300

    retirado mapper das env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import { env } from "./environment/env";
+
 
 dotenv.config();
 
 const app = express();
-
+                       
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 console.log("teste:" + JSON.stringify(process.env));
-console.log(env.TESTE);
+
 export default app;
--- backend/src/environment/env.ts
+++
@@ -1,7 +0,0 @@
-import {z} from "zod"
-
-const envSchema = z.object({
-    TESTE: z.string({message:"erro"}),
-});
-
-export const env = envSchema.parse(process.env);
