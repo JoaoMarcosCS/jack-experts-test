5f0db92a15a66e079be84c12a9e57e0723a856b8
JoaoMarcosCS <jmcsjoaomarcos>
2024-08-29 22:11:13 -0300

    chore: esquema de validaçao para a criacao do usuario

--- frontend/src/signUp/schemas/create-user.schema.ts
+++ frontend/src/signUp/schemas/create-user.schema.ts
@@ -1,7 +1,17 @@
 import { z } from "zod"
 
 export const CreateUserSchema = z.object({
+    name: z.string({
+        required_error: "Nome é obrigatório",
+        invalid_type_error: "Nome precisa ser um texto"
+      }).min(3, { message: "O nome precisa ter no mínimo 3 letras" }),
 
+      email: z.string({
+        required_error: "Email obrigatório"
+      }).email("Insira um email válido"),
+
+
+      password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
 })
 
 export type CreateUserProps = z.infer<typeof CreateUserSchema>
