53c1b0bf6220723dc689c6612e66b16ee87cfc87
JoaoMarcosCS <jmcsjoaomarcos@gmail.com>
2024-08-30 22:05:17 -0300

    fix: redirecionamento do usuário

--- frontend/src/signUp/hooks/useCreateUserMutate.ts
+++ frontend/src/signUp/hooks/useCreateUserMutate.ts
@@ -2,13 +2,19 @@ import { AxiosError } from "axios";
 import { useMutation } from "react-query";
 import { CreateUserService } from "../services/create-user.service";
 import { ErrorObject, useErrorState } from "../../store/user.store";
+import { useNavigate } from "react-router-dom"
+
 
 export function useCreateUserMutate(){
+    
     const { setError } = useErrorState();
+    const navigate = useNavigate()
+
     const mutate = useMutation({
         mutationFn: CreateUserService,
         onSuccess:(data: any) => {
-            console.log("Response success: \n\n" + JSON.stringify(data))
+            console.log("Response success: \n\n" + JSON.stringify(data));
+            navigate("/signup");
         },
         onError:(error: AxiosError) => {
             console.log("Response error\n\n: " + JSON.stringify(error.response?.data))
