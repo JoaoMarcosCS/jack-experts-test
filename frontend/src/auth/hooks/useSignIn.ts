7729e073bee552cd87a6582c27f640288f522f9c
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-09-02 09:55:10 -0300

    limpando o hook

--- frontend/src/auth/hooks/useSignIn.ts
+++ frontend/src/auth/hooks/useSignIn.ts
@@ -3,14 +3,14 @@ import { authSignin } from "../auth.service";
 import { AxiosError } from "axios";
 import { getApiMessageError } from "../../utils/getApiMessageError";
 import { toast } from "sonner";
+import { TokenResponse } from "../interfaces/tokenResponse.interface";
 
 export function useSignIn() {
 
     const mutate = useMutation({
         mutationFn: authSignin,
-        onSuccess: (data: any) => {
-            console.log("Response success: \n\n" + JSON.stringify(data))
-            toast(JSON.stringify(data));
+        onSuccess: (data: TokenResponse) => {
+            console.log("Response success: \n\n" + JSON.stringify(data));
         },
         onError: (error: AxiosError) => {
             console.log("Response error\n\n: " + JSON.stringify(error.response?.data))
