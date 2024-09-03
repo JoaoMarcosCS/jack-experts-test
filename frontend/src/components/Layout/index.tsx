e33c7e715ea5b8d1a0bcf812cd8f4856c5d0c306
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-09-03 17:07:58 -0300

    fix: add provider do react query

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -2,14 +2,17 @@ import { ReactNode } from "react"
 import { NavBar } from "./NavBar";
 import { SideMenu } from "./SideMenu";
 import { FooterMenu } from "./FooterMenu";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface RootLayoutProps {
     children: ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 export const RootLayout = ({ children }: RootLayoutProps) => {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <SideMenu />
             <main className="max-sm:ml-0 ml-[91px] bg-white">
                 <NavBar />
@@ -18,6 +21,6 @@ export const RootLayout = ({ children }: RootLayoutProps) => {
                 </div>
                 <FooterMenu />
             </main>
-        </>
+        </QueryClientProvider>
     )
 }
