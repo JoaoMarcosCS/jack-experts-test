21366264116eade4ae5ea6b79837a9c6cbc3b4ac
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-09-03 11:28:17 -0300

    chore: aprimorada a propriedade status de Task

--- backend/src/entities/task.entity.ts
+++ backend/src/entities/task.entity.ts
@@ -1,6 +1,12 @@
 import { BeforeUpdate, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 
+
+enum Status {
+    OPEN = 'open',
+    COMPLETED = 'completed',
+  }
+
 @Entity()
 export class Task {
 
@@ -19,8 +25,8 @@ export class Task {
     @ManyToOne(() => User, { cascade: true })
     user: User;
 
-    @Column({default: "open"})
-    status: string;
+    @Column({type: 'enum', enum: Status, default: Status.OPEN})
+    status: Status;
 
     @Column({default: false})
     isFavorite: boolean;
