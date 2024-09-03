24cce31dd302199592ec2a9fd738d8491fd16df4
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-09-03 11:31:44 -0300

    fix: propriedade status volta a ser string ao invés de enum

--- backend/src/entities/task.entity.ts
+++ backend/src/entities/task.entity.ts
@@ -1,12 +1,6 @@
 import { BeforeUpdate, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 
-
-enum Status {
-    OPEN = 'open',
-    COMPLETED = 'completed',
-  }
-
 @Entity()
 export class Task {
 
@@ -25,8 +19,8 @@ export class Task {
     @ManyToOne(() => User, { cascade: true })
     user: User;
 
-    @Column({type: 'enum', enum: Status, default: Status.OPEN})
-    status: Status;
+    @Column({default: "open"})
+    status: string;
 
     @Column({default: false})
     isFavorite: boolean;
