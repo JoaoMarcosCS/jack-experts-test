760813926b79a891325a51b749283903eb6fe799
JoaoMarcosCS <jmcsjoaomarcos@gmail.com>
2024-09-04 19:37:49 -0300

    style: tela de procura de task

--- frontend/src/search/index.tsx
+++ frontend/src/search/index.tsx
@@ -1,7 +1,45 @@
-export const Search = () => {
-    return(
-        <section className="">
+import { useState } from "react"
+import { useEffect } from "react"
+import { useSearchTask } from "./hooks/useSearchTask";
 
-        </section>
+export const Search = () => {
+    
+    const [searchText, setSearchText] = useState();
+
+    const {} = useSearchTask()
+
+    useEffect(() => {
+        
+    }, searchText)
+
+    return(
+        <div className="w-full pl-2 flex flex-col">
+            <div className="flex w-full justify-center items-center flex-col">
+                
+            </div>
+
+            <div className="flex w-full justify-center items-center gap-4 flex-wrap">
+                {isLoading && (
+                <>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                    <CardSkeleton/>
+                </>
+                )}
+                { !isLoading && data && (
+                    <>
+                    {data.tasks.map((task) => (
+                        <CardTask key={task.id} task={task}/>
+                    ))}
+                    </>
+                )}
+            </div>
+        </div>
     )
 }
