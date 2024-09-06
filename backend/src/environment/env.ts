ba979d0720aa433773e76ebd1557098a485c7ebd
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-09-06 20:39:40 -0300

    refact: retirado variavéis de ambiente não utilizadas

--- backend/src/environment/env.ts
+++ backend/src/environment/env.ts
@@ -7,8 +7,6 @@ const envSchema = z.object({
   APP_PORT: z.string(),
   TOKEN_SECRET: z.string(),
   TOKEN_EXPIRATION: z.string(),
-  DATABASE_URI: z.string()
-
 })
 
 export const env = envSchema.parse(process.env);
