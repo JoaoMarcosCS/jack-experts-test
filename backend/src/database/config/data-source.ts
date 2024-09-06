e046c1f3c83af59e6000026126917c235e6aa9a9
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-09-05 22:36:56 -0300

    refact: retirada propriedades e configurações de ambiente de desenvolvimento

--- backend/src/database/config/data-source.ts
+++ backend/src/database/config/data-source.ts
@@ -5,15 +5,17 @@ import { seeder } from "../seeds/seeder";
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "db.sqlite",
-    synchronize: true,
     entities: ["src/entities/*{.ts,.js}*"],
-    logging: true
+    //configuração de ambiente de desenvolvimento
+    // synchronize: true,
+    // logging: true
 })
 
 AppDataSource.initialize()
     .then(() => {
         console.log("Database connected");
-        seeder();
+        //configuração usada apenas no ambiente desenvolvimento
+        // seeder();
     })
     .catch((err) => {
         console.error("Database disconnected: ", err)
